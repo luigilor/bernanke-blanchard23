4e563b679e92b43b3e6f38c2aecf773cd22ea495
luigilor <pigio.lor@gmail.com>
2025-09-09 19:08:03 +0200

    Update version date in bl_be_us.R

--- empirics/code/bl_be_us.R
+++ empirics/code/bl_be_us.R
@@ -1,7 +1,7 @@
 #What Caused U.S. Postpandemic Inflation? A Fiscal Extension of Bernanke and Blanchard (2023)
 #Replication of Bernanke and Blanchard (2023)
 #Author: Luigi Lorenzoni
-#This version: 17/09/24
+#This version: 04/07/25
 
 
 ################################################### PREPARATION ####
